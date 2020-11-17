41dc1f2f7ae4aac9421bc976c346ae5bd443ff2a
HansMoura20 <hansadrienmoura@outlook.com>
2020-11-17 10:19:29 +0800

    Global styles and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { GlobalStyle } from './globalStyles';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+    <GlobalStyle />
+      <Navbar/>
+    </Router>
   );
 }
 
